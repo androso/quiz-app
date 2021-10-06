1b14310514d56ef1a52ecb749c56eee009d72b51
Anibal <androso421@gmail.com>
2021-10-06 11:53:38 -0600

    adding progressing bar and randomizing the displaying order of choices

--- js/modules/Quiz.js
+++ js/modules/Quiz.js
@@ -1,9 +1,11 @@
-import Quiz from "../utils/Quiz-constructor.js";
 import QuizConstructor from "../utils/Quiz-constructor.js";
+import Question from "../utils/Questions-constructor.js";
 import GameScreen from "../views/Game-screen.js";
 import { renderCustomizeMenu } from "./customize.js";
 
 const $appContainer = document.querySelector(".app");
+let quiz;
+let progressBarWidth = 0;
 
 export const renderQuiz = (numberOfQuestions, categoryId, difficulty) => {
 	getQuestionsData(numberOfQuestions, categoryId, difficulty);
@@ -17,65 +19,103 @@ const loadHTML = () => {
 const addListeners = () => {
 	const $nextButton = document.querySelector(".button.next");
 	const $restartButton = document.querySelector(".button.restart");
+	const $modalContainer = document.querySelector(".modal");
+
+	$modalContainer.addEventListener("click", (event) => {
+		closeModal(event, $modalContainer);
+	});
 	$nextButton.addEventListener("click", checkAnswer);
-	$restartButton.addEventListener("click", renderCustomizeMenu); 
-}
+	$restartButton.addEventListener("click", renderCustomizeMenu);
+};
+const closeModal = (event, $modalContainer) => {
+	if (
+		event.target.matches(".modal__message") ||
+		event.target.matches(".modal__body")
+	) {
+		return false;
+	}
+	$modalContainer.classList.toggle("show");
+};
+
 const checkAnswer = () => {
 	const $checkedLabel = document.querySelector(".quiz__input:checked + label");
 	const $alertModal = document.getElementsByClassName("modal")[0];
-	
+
 	if (!$checkedLabel) {
 		$alertModal.classList.toggle("show");
 		return false;
 	}
 	const answer = $checkedLabel.innerText;
-
-	console.log(answer);
-}
+	quiz.guess(answer);
+	renderNewQuestion(quiz);
+};
 
 const getQuestionsData = async (numberOfQuestions, categoryId, difficulty) => {
 	const $spinnerContainer = document.querySelector(".spinner-container");
 	const reqLink = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${categoryId}&difficulty=${difficulty}&type=multiple`;
+
 	$spinnerContainer.classList.toggle("spinner-container--active");
 	const questionsData = await (await fetch(reqLink)).json();
 	$spinnerContainer.classList.toggle("spinner-container--active");
+
 	const questions = questionsData.results.map((questionObject) => {
 		const question = questionObject.question;
 		const wrongChoices = questionObject.incorrect_answers;
 		const correctChoice = questionObject.correct_answer;
-		return {
-			question,
-			choices: [...wrongChoices, correctChoice],
-			correctAnswer: correctChoice,
-		};
+		const allChoices = shuffle([...wrongChoices, correctChoice]);
+		return new Question(question, allChoices, correctChoice);
 	});
-	const quiz = new QuizConstructor(questions);
-	renderNewQuestion(quiz);
-};
 
-const renderNewQuestion = (quiz) => {
+	quiz = new QuizConstructor(questions);
 	loadHTML();
 	addListeners();
+	renderNewQuestion(quiz);
+};
+const shuffle = (array) => {
+	let currentIndex = array.length;
+	let randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+	return array;
+}
+const renderNewQuestion = (quiz) => {
 	const $quizQuestion = document.querySelector(".quiz__question");
 	const $tracker = document.querySelector(".quiz__current-question");
 	const $tagline = document.querySelector(".quiz__tagline");
 	let $choices = Array.from(document.querySelectorAll(".quiz__label"));
-	const $innerProgress = document.querySelector(".quiz__progress-bar__inner");
 	const $checkedInput = document.querySelector(".quiz__input:checked");
 	const currentQuestion = quiz.getCurrentQuestion();
 	const currentQuestionIndex = quiz.getCurrentIndex();
+	const $nextButton = document.querySelector(".button.next");
 
-	if (quiz.currentIndex === quiz.questions.length) {
-		
+	if (quiz.hasEnded()) {
+		//Render results screen
+		$quizQuestion.innerText = "Geat Job!";
+		const finalScore = Math.floor((quiz.score * 100) / quiz.questions.length);
+		$tracker.innerText = `Your final score: ${finalScore}%`;
+		$tagline.innerText = "Completed!";
+		$nextButton.style.display = "none";
+		progressBarWidth = 0;
+		return true;
 	}
-	
+
 	if ($checkedInput) $checkedInput.checked = false;
 	$quizQuestion.innerHTML = currentQuestion.question;
 	$tracker.innerText = `${currentQuestionIndex} of ${quiz.questions.length}`;
+	incrementProgressBar();
+	$tagline.innerText = `Pick and option from below !`;
 	$choices.map(($choice, index) => {
 		$choice.innerHTML = `<i></i>${currentQuestion.choices[index]}`;
 	});
+};
 
+const incrementProgressBar = () => {
+	const $innerProgressBar = document.querySelector(".quiz__progress-bar__inner");
+	const increaseBarWidth = 100 / quiz.questions.length;
+	progressBarWidth+= increaseBarWidth;
+	$innerProgressBar.style.width = `${progressBarWidth}%`;
 }
-
-
